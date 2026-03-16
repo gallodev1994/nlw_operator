9eb3c71c80038bde8588797f92e6e8574742b9ca
Christian Gallo <contato.gallodev@gmail.com>
2026-03-16 17:13:02 -0300

    fix: pass context to createCaller

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,6 +1,9 @@
 import { appRouter } from "@/server/routers/_app";
-import { createCallerFactory } from "@/server/trpc";
+import { createCallerFactory, createTRPCContext } from "@/server/trpc";
 
-export const createCaller = createCallerFactory(appRouter);
+export async function createCaller() {
+  const ctx = await createTRPCContext();
+  return createCallerFactory(appRouter)(ctx);
+}
 
 export type { AppRouter } from "@/server/routers/_app";
